097b616e616618eebc01b977a436cba5237c3021
345161974 <ypf1990@foxmail.com>
2016-07-22 11:15:28 +0800

    build xtrader

---
+++ CppCTP_Integration/xTrader/CMakeFiles/quant_ctp_XTrader.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/hgfs/gmctp/CppCTP_Integration/xTrader/MdSpi.cpp" "/mnt/hgfs/gmctp/CppCTP_Integration/xTrader/CMakeFiles/quant_ctp_XTrader.dir/MdSpi.cpp.o"
+  "/mnt/hgfs/gmctp/CppCTP_Integration/xTrader/TdSpi.cpp" "/mnt/hgfs/gmctp/CppCTP_Integration/xTrader/CMakeFiles/quant_ctp_XTrader.dir/TdSpi.cpp.o"
+  "/mnt/hgfs/gmctp/CppCTP_Integration/xTrader/main.cpp" "/mnt/hgfs/gmctp/CppCTP_Integration/xTrader/CMakeFiles/quant_ctp_XTrader.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
